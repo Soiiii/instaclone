316e7a7d9e24dcbcae3a82cf14d7d15146257704
Soiiii <cojomkr33@gmail.com>
2024-11-08 23:47:44 +0900

    db update

--- content/views.py
+++ content/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed
+        return render(request, 'instaclone/main.html', context=dict(feeds=feed_list))
